e62f05c98f433c40e5e9ac934d8b23bd95d70dbe
EPAMI <90223377+EPAMI@users.noreply.github.com>
2024-11-06 21:24:22 +0100

    Card for Book

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
-
-
+import rooms from '@/data/rooms.json';
+import RoomCard from './components/RoomCard';
 export default function Home() {
   return (
     <>
-    <h1>Welcome to Book-it!</h1>
+      {rooms.length > 0 ? (
+        rooms.map((room) => <RoomCard room={room}/>)
+      ) : (<p>Aucune salle disponible pour le moment</p>)}
     </>
   );
 }
